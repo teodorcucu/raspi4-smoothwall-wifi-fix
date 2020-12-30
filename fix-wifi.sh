2e9ae7eb119b7fd538c71ab18cd004b719f0e6e8
Teodor Cucu <teodorcucu99@gmail.com>
2020-12-30 09:03:09 +0100

    Update fix-wifi.sh

--- fix-wifi.sh
+++ fix-wifi.sh
@@ -8,14 +8,16 @@ echo -e "Cloning fixed hostapd.purple.conf from GitHub"
 wget 
 https://github.com/teodorcucu/raspi4-smoothwall-wifi-fix/blob/main/hostapd.purple.conf
 
+echo -e "CD in /etc/hostapd/"
 echo -e "Backup the old hostapd.purple.conf as 
 hostapd.purple.conf.original"
-cp /etc/hostapd/hostapd.purple.conf /etc/hostapd/hostapd.purple.conf.original
+cp hostapd.purple.conf hostapd.purple.conf.original
 
 echo -e "Remove the old hostapd.purple.conf"
-rm -rf /etc/hostapd/hostapd.purple.conf
+rm -rf hostapd.purple.conf
 
 echo -e "Copy the new one to /etc/hostapd/"
+cd /tmp/fix-wifi/
 cp hostapd.purple.conf /etc/hostapd/hostapd.purple.conf
 
 echo -e "Setting up PURPLE_WIFI Variable as eth2 in 
