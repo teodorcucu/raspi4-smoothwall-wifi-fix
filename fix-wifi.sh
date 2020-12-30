3b83e9d1efa424bf2c299d71125c321f45a9126a
Teodor Cucu <teodorcucu99@gmail.com>
2020-12-30 10:02:33 +0100

    Update fix-wifi.sh

--- fix-wifi.sh
+++ fix-wifi.sh
@@ -1,40 +1,47 @@
 #!/bin/bash
 
-echo -e "Creating tmp dir"
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
+echo -e "${RED}Creating tmp dir${NOCOLOR}"
 mkdir /tmp/fix-wifi/
 cd /tmp/fix-wifi/
+echo -e "${GREEN}DONE.${NOCOLOR}"
 echo -e
-echo -e "Cloning fixed hostapd.purple.conf from GitHub"
+
+echo -e "${RED}Cloning fixed hostapd.purple.conf from GitHub${NOCOLOR}"
 wget https://raw.githubusercontent.com/teodorcucu/raspi4-smoothwall-wifi-fix/main/hostapd.purple.conf
-echo -e
-
-echo -e "CD in /etc/hostapd/"
 cd /etc/hostapd/
+echo -e "${GREEN}DONE.${NOCOLOR}"
 echo -e
 
-echo -e "Backup the old hostapd.purple.conf as
-hostapd.purple.conf.original"
+echo -e "${RED}Backup the old hostapd.purple.conf as hostapd.purple.conf.original${NOCOLOR}"
 cp hostapd.purple.conf hostapd.purple.conf.original
+echo -e "${GREEN}DONE.${NOCOLOR}"
 echo -e
 
-echo -e "Remove the old hostapd.purple.conf"
+echo -e "${RED}Remove the old hostapd.purple.conf${NOCOLOR}"
 rm -rf hostapd.purple.conf
+echo -e "${GREEN}DONE.${NOCOLOR}"
 echo -e
 
-echo -e "Copy the new one to /etc/hostapd/"
+echo -e "${RED}Copy the new one to /etc/hostapd/${NOCOLOR}"
 cd /tmp/fix-wifi/
 cp hostapd.purple.conf /etc/hostapd/hostapd.purple.conf
+echo -e "${GREEN}DONE.${NOCOLOR}"
 echo -e
 
-echo -e "Setting up PURPLE_WIFI Variable as eth2 in
-/var/smoothwall/ethernet/settings"
+echo -e "${RED}Setting up PURPLE_WIFI Variable as eth2 in /var/smoothwall/ethernet/settings${NOCOLOR}"
 echo -e "PURPLE_WIFI=eth2" >>/var/smoothwall/ethernet/settings
+echo -e "${GREEN}DONE.${NOCOLOR}"
 echo -e
 
-echo -e "Add /usr/sbin/wifi start purple to /etc/rc.d/rc.sysinit"
+echo -e "${RED}Add /usr/sbin/wifi start purple to /etc/rc.d/rc.sysinit${NOCOLOR}"
 cd /etc/rc.d/
 sed -i '$d' rc.sysinit
 echo -e "/usr/sbin/wifi start purple" >>/etc/rc.d/rc.sysinit
 echo -e "exit 0" >>/etc/rc.d/rc.sysinit
+echo -e "${GREEN}DONE.${NOCOLOR}"
 echo -e
-echo -e "Done, reboot now"
+echo -e "${GREEN} Reboot now!${NOCOLOR}"
